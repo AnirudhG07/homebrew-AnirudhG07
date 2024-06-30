41e92cbdc570b5a5aa754f1606af1521edf07b91
AnirudhG07 <anirudhgupta@iisc.ac.in>
2024-06-30 13:24:47 +0530

    cheatshh1.1.1 issues
    solved hopefully who knows

--- Formula/cheatshh.rb
+++ Formula/cheatshh.rb
@@ -2,7 +2,7 @@ class Cheatshh < Formula
     desc "Cheatshh is a CLI tool to store commands and their descriptions in a place you can look into as cheatshheat so you dont have to remember them."
     homepage "https://github.com/AnirudhG07/cheatshh"
     url "https://github.com/AnirudhG07/cheatshh/releases/download/v1.1.1/cheatshh-1.1.1.tar.gz"
-    sha256 "65169ea0fa0d0bf239a5a420a3446f2a9abf8457542c9ea732b43653984f6d04"
+    sha256 "1e8e1b888ccde73686fb43e1c369c0b07a674109149c5ab47e7b1a0b5c733923"
     version "1.1.1"
     license "Apache-2.0"
 
@@ -14,6 +14,11 @@ class Cheatshh < Formula
     resource "whiptail" do
         url "https://files.pythonhosted.org/packages/source/w/whiptail/whiptail-0.2.tar.gz"
         sha256 "c3bd05674c8c22b92a0da47f0bb6cfd2da0601e69bd09b3077c92a19d8853c09"
+    end
+
+    resource "setuptools" do
+        url "https://files.pythonhosted.org/packages/source/s/setuptools/setuptools-70.1.0.tar.gz"
+        sha256 "01a1e793faa5bd89abc851fa15d0a0db26f160890c7102cd8dce643e886b47f5"
     end
 
     def install

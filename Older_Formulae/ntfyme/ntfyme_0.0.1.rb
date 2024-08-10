7f33338996fd22a28ca61aed599e1ff636e5220b
AnirudhG07 <anirudhgupta@iisc.ac.in>
2024-08-11 00:13:55 +0530

    ntfyme v0.0.2 bump

--- Older_Formulae/ntfyme/ntfyme_0.0.1.rb
+++ Older_Formulae/ntfyme/ntfyme_0.0.1.rb
@@ -1,8 +1,8 @@
 class Ntfyme < Formula
     desc "A simple notification tool to notify yourself when a long running process ends with local ping, gmail, telegram, etc."
     homepage "https://github.com/AnirudhG07/ntfyme"
-    url "https://github.com/AnirudhG07/ntfyme/releases/download/v0.0.2/ntfyme-0.0.2.tar.gz"
-    sha256 "f20ed27dbe431372e08a636e71140065bc55db92f98da3bc95e7a25d6432e84e"
+    url "https://github.com/AnirudhG07/ntfyme/releases/download/v0.0.1/ntfyme-0.0.1.tar.gz"
+    sha256 "5afa7918671bbd627a1978db21b9bd594549e5bbdc218e12f3c082a84651ada2"
     license "Apache-2.0"
 
     depends_on "python@3"
@@ -32,20 +32,6 @@ class Ntfyme < Formula
         sha256 "65b7dfb7e11e07af37a8487eb2aa69524276ef70dad500b07228ce64736baa61"
     end 
 
-    resource "rich" do
-        url "https://files.pythonhosted.org/packages/source/r/rich/rich-13.7.0.tar.gz"
-        sha256 "9be308cb1fe2f1f57d67ce99e95af38a1e2bc71ad9813b0e247cf7ffbcc3a432"
-    end
-
-    resource "beaupy" do
-        url "https://files.pythonhosted.org/packages/source/b/beaupy/beaupy-3.9.1.tar.gz"
-        sha256 "4d9655aa93c099f12291290d8340dfb71796ab27a1706ce41c736c451cb9b88e"
-    end
-
-    resource "rich-click" do
-        url "https://files.pythonhosted.org/packages/source/r/rich-click/rich-click-1.8.3.tar.gz"
-        sha256 "6d75bdfa7aa9ed2c467789a0688bc6da23fbe3a143e19aa6ad3f8bac113d2ab3"
-    end
 
     def install
         system "pip3", "install", ".", "--prefix=#{libexec}"
